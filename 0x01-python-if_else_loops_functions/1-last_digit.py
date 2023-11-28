bd8cf448ea127c3656fb9d82d1d9d5ef28e46148
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-28 12:13:20 -0800

    Add solution for Task 1: Last digit program

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+string = "Last digit of {} is {}".format(number, last_digit)
+
+if last_digit > 5:
+    print("{} and is greater than 5".format(string))
+elif last_digit == 0:
+    print("{} and is 0".format(string))
+else:
+    print("{} and is less than 6 and not 0".format(string))
