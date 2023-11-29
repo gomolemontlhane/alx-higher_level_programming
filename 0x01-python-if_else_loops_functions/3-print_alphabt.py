73dbcfb6ec6d533006cea0f452aa4dac6e574893
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-28 19:02:28 -0800

    adding files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ['q', 'e']:
-        print(chr(char), end='')
-
-print()  # Print a new line at the end
+for char in "abcdefghijklmnopqrstuvwxyz":
+    if char != 'q' and char != 'e':
+        print("{}".format(char), end="")
+print()
