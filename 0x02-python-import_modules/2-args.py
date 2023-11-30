a903ee38339e735b2d533a18536c6753a2941255
gomolemontlhane <gomolemontlhane@gmail.com>
2023-11-30 11:54:25 -0800

    updating

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 
-num_args = len(argv) - 1
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
 
-if num_args == 0:
-    print("0 arguments.")
-elif num_args == 1:
-    print("1 argument:")
-else:
-    print(f"{num_args} arguments:")
-
-for i in range(1, num_args + 1):
-    print(f"{i}: {argv[i]}")
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
