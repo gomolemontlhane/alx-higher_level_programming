1a8da46c4df85ed8b66b668202dc091a806f7651
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-04 19:29:28 -0800

    Add solution for Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,59 @@
-#include <stddef.h>
 #include "lists.h"
+#include <stddef.h>
+/**
+ *reverse_list - reverses a linked list
+ *@head: pointer to the head of the list
+ *
+ *Return: pointer to the new head of the reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
 
 /**
- *is_palindrome - checks if a singly linked list is a palindrome
- *@head: pointer to the head of the linked list
- *Return: 1 if palindrome, 0 otherwise
+ *is_palindrome - checks if a linked list is a palindrome
+ *@head: pointer to the head of the list
+ *
+ *Return: 1 if the list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	listint_t *slow = *head, *fast = *head;
-	listint_t *prev = NULL, *temp = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
 
 	while (fast != NULL && fast->next != NULL)
 	{
+		slow = slow->next;
 		fast = fast->next->next;
-
-		temp = slow->next;
-		slow->next = prev;
-		prev = slow;
-		slow = temp;
 	}
 
-	if (fast != NULL)
-		slow = slow->next;
+	slow = reverse_list(&slow);
+	fast = *head;
 
 	while (slow != NULL)
 	{
-		if (prev->n != slow->n)
+		if (slow->n != fast->n)
 			return (0);
 
-		prev = prev->next;
 		slow = slow->next;
+		fast = fast->next;
 	}
 
 	return (1);
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 /**
  *struct listint_s - singly linked list
@@ -13,9 +14,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}
-
-listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
