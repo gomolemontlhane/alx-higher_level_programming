45d5657aeb4b30d4f704437e113aaf6f4f106382
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-06 06:52:24 -0800

    Implement Task 5: Number of Keys in Dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function to return the number of keys in a dictionary
+"""
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary"""
+    return len(a_dictionary)
