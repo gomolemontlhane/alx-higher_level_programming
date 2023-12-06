1a85ce446fe3e01bcc496d73e490b71776e12802
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-06 06:48:35 -0800

    Implement Task 4: Only Different Elements in Sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function to return a set of all elements present in only one set
+"""
+
+
+def only_diff_elements(set_1, set_2):
+    """Return a set of elements present in only one set"""
+    return set_1.symmetric_difference(set_2)
