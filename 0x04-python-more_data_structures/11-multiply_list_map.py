9cd8c5ac7a708d55c8df8ae1e9b507884e21466c
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-06 07:40:51 -0800

    Implement Task 11: Multiply List Values by a Number using Map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""
-Function to return a new list with all values multiplied by a number using map
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    """Return a new list with all values multiplied by a number using map"""
     return list(map(lambda x: x * number, my_list))
