67cba8b2a2ddaff15509ab8949b655c6a1f03420
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-19 15:08:50 -0800

    Task 4: Add property getters and setters for size in Square class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a private instance attribute size,
+size validation, a public instance method area, and property getters and
+setters for the size attribute.
+"""
+
+
+class Square:
+    """
+    Square class with a private instance attribute size, size validation,
+    a public instance method area, and property getters and setters for
+    the size attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
