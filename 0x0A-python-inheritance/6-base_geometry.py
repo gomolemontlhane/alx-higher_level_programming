020a57baedc6bbc93635d151a7c90f15417493cb
gomolemontlhane <gomolemontlhane@gmail.com>
2024-01-08 17:21:33 -0800

    edited to docstyle conventions

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Module that defines the improved BaseGeometry class.
+"""
 
 class BaseGeometry:
-    """A class representing the base geometry."""
+    """
+    Improved BaseGeometry class with an area() method.
+    """
 
     def area(self):
-        """Raises an Exception with the message 'area() is not implemented'."""
+        """
+        Raises an Exception with the message area() is not implemented.
+        """
         raise Exception("area() is not implemented")
-
 
 if __name__ == "__main__":
     bg = BaseGeometry()
@@ -15,3 +21,4 @@ if __name__ == "__main__":
         print(bg.area())
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
+
