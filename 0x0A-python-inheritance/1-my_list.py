0035fb874a46fc48bfd4798613162861d7287a65
gomolemontlhane <gomolemontlhane@gmail.com>
2024-01-08 17:35:00 -0800

    edited to docstyle conventions

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
 """
-Module that defines the MyList class.
+Module that defines the MyList class, which inherits from list.
 """
 
 class MyList(list):
     """
-    MyList class, inherits from the list class.
-
-    Public instance method:
-    - def print_sorted(self): prints the list in ascending order.
+    MyList class that inherits from list.
     """
 
     def print_sorted(self):
         """
-        Print the list in ascending order.
+        Prints the list in ascending order.
         """
         print(sorted(self))
 
 
 if __name__ == "__main__":
+    # Test the MyList class
     my_list = MyList()
     my_list.append(1)
     my_list.append(4)
     my_list.append(2)
     my_list.append(3)
     my_list.append(5)
-
     print(my_list)
     my_list.print_sorted()
     print(my_list)
