6b9f28a589db2bd81b2ac48281eb0fc37f68065c
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-15 15:45:33 -0800

    correcting 15 (removing hbtn_0c_0)

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- List number of records for each score
-SELECT score, COUNT(*) AS number FROM hbtn_0c_0.second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
 
