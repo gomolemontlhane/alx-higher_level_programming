412ffdd7fb3fb401700d53f5f2c26ac3b0421763
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-15 15:46:43 -0800

    correcting 15 (removing hbtn_0c_0)

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List all records with names, ordered by descending score
-SELECT score, name FROM hbtn_0c_0.second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
 
