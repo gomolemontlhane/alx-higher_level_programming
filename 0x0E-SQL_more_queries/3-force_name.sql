97ecd1e5a96396c28465908b037e51568c8a65bd
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-14 16:18:27 -0800

    Add SQL script for creating table force_name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,9 @@
+-- Create the table force_name if it doesn't exist
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
+
+-- Display the table structure
+DESCRIBE force_name;
+
