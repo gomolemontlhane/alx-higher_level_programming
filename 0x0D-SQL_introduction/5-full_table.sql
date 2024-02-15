ba1e94bc9cdb46138572fcd62b8d18cdfbc7b56a
gomolemontlhane <gomolemontlhane@gmail.com>
2024-02-15 14:11:23 -0800

    correcting 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
--- Show full table description
-DESCRIBE hbtn_0c_0.first_table;
+-- Print the full description of the table first_table
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
 
