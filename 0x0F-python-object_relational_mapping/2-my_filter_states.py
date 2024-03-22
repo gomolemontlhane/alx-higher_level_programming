bae344dc493e50a7ca02370182351e586aa13b4c
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-22 14:05:45 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,33 @@
-
-2. **2-filter_states.py**: This Python script retrieves and filters states from a MySQL database based on user input.
-
-```python
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
 """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Database connection parameters
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     # Connect to MySQL database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
+    # Create cursor to execute queries
+    cur = db.cursor()
 
-    # Execute SQL query to retrieve states based on user input
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cursor.execute(query, (sys.argv[4],))
+    # Execute query
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
 
-    # Fetch all the rows in a list of lists
-    rows = cursor.fetchall()
-
-    # Print the state information
-    for row in rows:
+    # Fetch all rows and print
+    for row in cur.fetchall():
         print(row)
 
-    # Close cursor and database connection
-    cursor.close()
+    # Close cursor and database
+    cur.close()
     db.close()
 
