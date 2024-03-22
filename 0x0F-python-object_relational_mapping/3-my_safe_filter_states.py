c0a0821929f5ef21ba9922b15e492481fbbeae7d
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-22 14:08:41 -0700

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-import sys
+"""
+Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+This version is safe from SQL injection.
+"""
+
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        sys.exit(1)
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    # Extract arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Create cursor to execute queries
+    cur = db.cursor()
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    # Execute query with parameters to avoid SQL injection
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
 
-    # Create cursor object
-    cursor = db.cursor()
+    # Fetch all rows and print
+    for row in cur.fetchall():
+        print(row)
 
-    # Prepare SQL query using parameterized query
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    try:
-        # Execute SQL query with user input as parameters
-        cursor.execute(query, (state_name,))
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-        # Display results
-        for row in results:
-            print(row)
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        # Close database connection
-        db.close()
+    # Close cursor and database
+    cur.close()
+    db.close()
 
