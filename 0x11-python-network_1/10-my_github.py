5fa0142d169108f6c9d97fca30b62eb1d9bccaf8
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-28 15:02:48 -0700

    Task 9: Add Python script for fetching GitHub user id using requests

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password) and uses the GitHub API
+to display your id.
+
+Uses Basic Authentication with a personal access token as password to access
+to your information (only read:user permission is needed)
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print("None")
