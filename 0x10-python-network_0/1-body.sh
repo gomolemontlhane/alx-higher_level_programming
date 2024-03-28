5de1ce9a11161b733846871974a85e3c4f66d4a7
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-27 20:08:43 -0700

    altered task 1 file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response for a 200 status code
-curl -s "$1" -X GET -o /dev/null -w "%{http_code}" | {
-    read status_code
-    if [ $status_code -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
-
+# Takes in a URL, sends a GET request to the URL, displays the body of the response
+curl -sX GET "$1" -L 200
