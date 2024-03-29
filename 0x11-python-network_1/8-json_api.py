60b7a00eceb10c21d5ac7e9a8c080af76147f400
gomolemontlhane <gomolemontlhane@gmail.com>
2024-03-28 17:22:14 -0700

    fixing task 8 file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-Script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
+Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty, display the id and
+name like this: [<id>] <name>
+Otherwise:
+    Display Not a valid JSON if the JSON is invalid
+    Display No result if the JSON is empty
 """
-import sys
-import requests
 
-def search_user(letter):
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
     payload = {'q': letter}
-    response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    response = requests.post(url, data=payload)
+
     try:
-        response_json = response.json()
-        if response_json:
-            print("[{}] {}".format(response_json.get("id"), response_json.get("name")))
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
         else:
             print("No result")
     except ValueError:
         print("Not a valid JSON")
-
-if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user(letter)
