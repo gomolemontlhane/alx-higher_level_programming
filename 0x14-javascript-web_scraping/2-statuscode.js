fc74ee808f4e55db3899cfbf8032ea5d7f43c3f6
root <gomolemontlhane@gmail.com>
2024-04-23 19:57:35 -0700

    fxing errors

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
+// Status Of Get request
 
 const request = require('request');
-
-// Get the URL from the command-line arguments
 const url = process.argv[2];
-
-// Make a GET request to the specified URL
-request(url, (error, response) => {
+request.get(url, function (error, response) {
   if (error) {
-    console.error(error);
-    return;
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
   }
-  
-  // Print the status code
-  console.log(`code: ${response.statusCode}`);
 });
